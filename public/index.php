0c93009d9bfbf8228f07d68f45264687637873cb
sam <samuela@hackeruso.com>
2021-12-29 23:39:34 +0200

    Added public/index.php as a placeholder for default file

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "<h1>Hello, I'm a Laravel-Docker!</h1>";
+echo "<p>A pretty simple Docker Compose workflow that sets up your own network of containers for local Laravel development.!</p>";
